632a96d76320320cd9643f337f4484aea3a92557
Cory Tripathy <ctripathy0@gmail.com>
2023-06-29 18:13:10 -0400

    fixed double tap delete button bug

--- TaskTamer/View/SortListView/SortListDisclosure.swift
+++ TaskTamer/View/SortListView/SortListDisclosure.swift
@@ -59,7 +59,9 @@ struct SortListDisclosure: View {
                 HStack {
                     Color.clear
                     Button {
-                        delete()
+                        if deleteModeEnabled {
+                            delete()
+                        }
                     } label: {
                         Rectangle()
                             .scaleEffect(y: yFrame)
@@ -153,13 +155,13 @@ struct SortListDisclosure: View {
     }
     
     func delete() {
+        deleteModeEnabled = false
         withAnimation(.easeIn(duration: 0.25)) { xOffset = geo.size.width }
         withAnimation(.easeIn(duration: 0.25)) { yFrame = 0 }
         vm.tasks.removeAll { $0 == task }
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
             xOffset = 0
             yFrame = 1
-            deleteModeEnabled = false
             taskDeleting = nil
         }
     }
