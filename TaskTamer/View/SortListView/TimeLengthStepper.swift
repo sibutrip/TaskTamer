677b75235195cf3434f6d5d04b37a0f496d71c38
Cory Tripathy <ctripathy0@gmail.com>
2023-07-08 18:52:23 -0400

    time stepper!!!

--- TaskTamer/View/SortListView/TimeLengthStepper.swift
+++ TaskTamer/View/SortListView/TimeLengthStepper.swift
@@ -8,37 +8,65 @@
 import SwiftUI
 
 struct TimeLengthStepper: View {
+    @Environment(\.dynamicTypeSize) var textSize
     @Binding var sliderValue: Int
     let geo: GeometryProxy
     @ScaledMetric(relativeTo: .body) var scaledPadding: CGFloat = 10
+    @State var hours: String
+    @State var mins: String
+    init(sliderValue: Binding<Int>, geo: GeometryProxy) {
+        _sliderValue = sliderValue
+        self.geo = geo
+        _hours = State<String>.init(initialValue: String(sliderValue.wrappedValue / 60))
+        _mins = State<String>.init(initialValue: String(sliderValue.wrappedValue % 60))
+    }
+    
+    func update(with newValue: Int) {
+        let mins = newValue % 60
+        let hours = newValue / 60
+        withAnimation(Animation.easeInOut(duration: 0.2)) {
+            self.hours = String(hours)
+            self.mins = String(mins)
+        }
+        self.sliderValue = newValue
+    }
+    
+    
+    
     var body: some View {
         HStack {
             Button {
-                print("ah")
+                let newValue = sliderValue - 15
+                update(with: newValue)
             } label: {
                 ZStack {
-                    label
+                    Text("")
+                        .padding(.vertical, scaledPadding * 0.5)
                         .opacity(0.0)
                     Image(systemName: "minus")
                         .fontWeight(.regular)
                         .frame(width: geo.size.width / 5)
                 }
             }
+            .disabled(sliderValue == 15)
             Divider()
             label
-                .frame(width: geo.size.width / 5)
+                .frame(width: geo.size.width / 4)
             Divider()
             Button {
-                print("ah")
+                let newValue = sliderValue + 15
+                update(with: newValue)
             } label: {
                 ZStack {
-                    label
+                    Text("")
+                        .padding(.vertical, scaledPadding * 0.5)
                         .opacity(0.0)
                     Image(systemName: "plus")
                         .fontWeight(.regular)
                         .frame(width: geo.size.width / 5)
                 }
             }
+            .disabled(sliderValue == 240)
         }
         .padding(.vertical, scaledPadding * 0.5)
         .background {
@@ -48,13 +76,31 @@ struct TimeLengthStepper: View {
         
     }
     var label: some View {
-        VStack {
-            Text("\(sliderValue)")
-            Text("hours")
-                .foregroundColor(.secondary)
-                .font(.caption)
-                .lineLimit(1)
-                .minimumScaleFactor(0.5)
+        let layout = textSize < .xLarge ? AnyLayout(HStackLayout()) : AnyLayout(VStackLayout())
+        return layout {
+            Group {
+                if hours != "0" {
+                    VStack {
+                        Text("\(hours)")
+                            .lineLimit(1)
+                        Text(hours == "1" ? "hour" : "hours")
+                            .foregroundColor(.secondary)
+                            .font(.caption)
+                            .lineLimit(1)
+                    }
+                }
+                if mins != "0" {
+                    VStack {
+                        Text("\(mins)")
+                            .lineLimit(1)
+                        Text("minutes")
+                            .foregroundColor(.secondary)
+                            .font(.caption)
+                            .lineLimit(1)
+                    }
+                }
+            }
+            .transition(.scale)
         }
     }
 }
