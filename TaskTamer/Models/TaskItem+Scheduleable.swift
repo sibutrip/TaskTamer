7f0ddafd3e0df8bc1d38413929eab03f6d18cc20
Cory Tripathy <ctripathy0@gmail.com>
2023-11-01 11:06:19 -0400

    added files

---
+++ TaskTamer/Models/TaskItem+Scheduleable.swift
@@ -0,0 +1,8 @@
+//
+//  TaskItem+Scheduleable.swift
+//  TaskTamer
+//
+//  Created by Cory Tripathy on 10/31/23.
+//
+
+import Foundation
---
+++ TaskTamer/TaskTamerApp.swift
@@ -0,0 +1,8 @@
+//
+//  TaskTamerApp.swift
+//  TaskTamer
+//
+//  Created by Cory Tripathy on 10/31/23.
+//
+
+import Foundation
