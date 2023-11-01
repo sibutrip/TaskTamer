42e9219bde1e10cf8fd4530bc4230b9b97075710
Cory Tripathy <ctripathy0@gmail.com>
2023-11-01 11:06:42 -0400

    moved taskitem+sched file location

--- TaskTamer/Models/TaskItem+Scheduleable.swift
+++ TaskTamer/Models/TaskItem+Scheduleable.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol Scheduleable {
+    
+    /// The title for the calendar item.
+    var eventTitle: String { get }
+    
+    /// The start time of the task
+    var startDate: Date? { get set }
+    
+    /// The end time of the task
+    var endDate: Date? { get set }
+    
+    /// The unique ID of the event from the event's EKEventStore
+    var eventID: String? { get set }
+}
